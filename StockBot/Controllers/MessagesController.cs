fcb67c83096e2dd643f2f30e699fa3eb986841ec
parthsr <srivastavaparth2016@gmail.com>
2017-01-29 18:42:01 +0530

    Add files via upload

---
+++ StockBot/Controllers/MessagesController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace StockBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+       
+
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            Activity reply;
+            if (activity.Type == ActivityTypes.Message)
+            {
+              
+                StockBot.Rootobject stLuis = await StockBot.LUISStockClient.ParseUserInput(activity.Text);
+                string strRet = string.Empty;
+                string strStock = activity.Text;
+                
+
+                // Get the stateClient to get/set Bot Data
+                StateClient _stateClient = activity.GetStateClient();
+                BotData _botData = _stateClient.BotState.GetUserData(activity.ChannelId, activity.Conversation.Id);
+
+                switch (stLuis.intents[0].intent)
+                {
+                    case "StockPrice":
+                        strRet = await YahooStock.Yahoo.GetStock(stLuis.entities[0].entity);
+                        break;
+                    case "None":
+                        strRet = "Sorry, I don't understand, perhaps try something like \"Show me Microsoft stock\"";
+                        break;
+                    default:
+                        break;
+                }
+
+                reply = activity.CreateReply(strRet);
+                // return our reply to the user
+
+            }
+            else
+            {
+                reply=HandleSystemMessage(activity);
+            }
+           
+            await connector.Conversations.ReplyToActivityAsync(reply);
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+
+                Activity reply= message.CreateReply("Hello sir, How may I help you?");
+                return reply;
+
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+
+
+
+
+    }
+
+}
---
+++ StockBot/Luis.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StockBot
+{
+    public class LUISStockClient
+    {
+        public static async Task<Rootobject> ParseUserInput(string strInput)
+        {
+            string strRet = string.Empty;
+            string strEscaped = Uri.EscapeDataString(strInput);
+
+            using (var client = new HttpClient())
+            {
+                string uri = "https://api.projectoxford.ai/luis/v2.0/apps/a7be928c-14db-4c0b-86ff-d492200428a9?subscription-key=caf34c8bbdec40178df501e2c171654a&q=" + strEscaped;
+                uri = uri + "&verbose=true";
+                HttpResponseMessage msg = await client.GetAsync(uri);
+                if (msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<Rootobject>(jsonResponse);
+                    return _Data;
+                }
+            }
+            return null;
+        }
+    }
+    public class Rootobject
+    {
+        public string query { get; set; }
+        public Topscoringintent topScoringIntent { get; set; }
+        public Intent[] intents { get; set; }
+        public Entity[] entities { get; set; }
+    }
+
+    public class Topscoringintent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public float score { get; set; }
+    }
+
+}
